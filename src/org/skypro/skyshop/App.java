a760c0d3a09d5077b9b8c3ab68603335a1617340
ArtemGSA <ArtemGSA@yandex.ru>
2025-03-04 05:30:21 +0300

    ff

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,17 +5,6 @@ import org.skypro.skyshop.product.Product;
 
 public class App {
     public static void main(String[] args) {
-        ProductBasket testPB = new ProductBasket();
-        String s = "ggg";
-        int u = 100;
-        for (int i = 0; i<6; i++){
-            Product tt = new Product(s, u+i);
-            testPB.addProduсt(tt);
-        }
-
-        testPB.getBasket();
-        System.out.println(testPB.prodInBasket("ggg"));
-        System.out.println(testPB.prodInBasket("ggggg"));
-        testPB.clearBasket();
+        System.out.println("Hi");
     }
 }
